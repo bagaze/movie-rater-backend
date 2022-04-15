3a34f4e1efff57e55c9aeabec7fcd53f80de9983
Baptiste GAZE <bpt.gaze@gmail.com>
2022-04-15 11:45:22 +0200

    Completed tests for users/admins token retrieval and usage

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -2,10 +2,16 @@ from databases import Database
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 import pytest
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_201_CREATED
+from starlette.status import (
+    HTTP_404_NOT_FOUND,
+    HTTP_201_CREATED,
+    HTTP_200_OK,
+    HTTP_401_UNAUTHORIZED
+)
 
 from app.crud.users import UserCrud
 from app.schemas.user import UserCreate, UserInDB, UserPublic
+from app.schemas.token import AccessToken
 
 
 @pytest.fixture
@@ -92,3 +98,181 @@ class TestUsersAPICreation:
         new_user[attr] = value
         res = client.post(app.url_path_for("users:post-user"), json=new_user)
         assert res.status_code == status_code
+
+
+@pytest.fixture
+def user_test_login():
+    return UserCreate(
+        email='jane.doe@mail.com',
+        username='jane_doe',
+        password='password'
+    )
+
+
+class TestUsersAPILogin:
+
+    async def test_user_login(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_login: UserCreate,
+        user_crud: UserCrud
+    ):
+        await user_crud.create_new_user(new_user=user_test_login, is_superuser=False)
+
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={'username': user_test_login.email, 'password': user_test_login.password}
+        )
+
+        token = AccessToken(**res.json())
+        assert token.token_type == 'Bearer'
+        assert token.access_token is not None
+
+    def test_user_get_me(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_login: UserCreate
+    ):
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={'username': user_test_login.email, 'password': user_test_login.password}
+        )
+        assert res.status_code == HTTP_201_CREATED
+        token = AccessToken(**res.json())
+
+        headers = {
+            'Authorization': f'{token.token_type} {token.access_token}'
+        }
+        res = client.get(
+            app.url_path_for("users:get-user-me"),
+            headers=headers
+        )
+        assert res.status_code == HTTP_200_OK
+
+        user_me = UserPublic(**res.json())
+        assert user_test_login.username == user_me.username
+        assert user_test_login.email == user_me.email
+
+    def test_token_not_existing_user(
+        self,
+        app: FastAPI,
+        client: TestClient,
+    ):
+        fake_user = {
+            'email': 'fake_user@mail.com',
+            'username': 'fake_user',
+            'password': 'fake_pass'
+        }
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={'username': fake_user['email'], 'password': fake_user['password']}
+        )
+        assert res.status_code == HTTP_401_UNAUTHORIZED
+
+    def test_user_access_prevention(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_login: UserCreate
+    ):
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={'username': user_test_login.email, 'password': user_test_login.password}
+        )
+        assert res.status_code == HTTP_201_CREATED
+        token = AccessToken(**res.json())
+
+        headers = {
+            'Authorization': f'{token.token_type} {token.access_token}'
+        }
+        res = client.get(
+            app.url_path_for("users:get-users"),
+            headers=headers
+        )
+        assert res.status_code == HTTP_401_UNAUTHORIZED
+
+
+@pytest.fixture
+def user_test_admin_login():
+    return UserCreate(
+        email='admin@mail.com',
+        username='admin',
+        password='password'
+    )
+
+
+class TestUsersAdminAPILogin:
+
+    async def test_admin_login(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_admin_login: UserCreate,
+        user_crud: UserCrud
+    ):
+        await user_crud.create_new_user(new_user=user_test_admin_login, is_superuser=True)
+
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={
+                'username': user_test_admin_login.email, 'password': user_test_admin_login.password
+            }
+        )
+
+        token = AccessToken(**res.json())
+        assert token.token_type == 'Bearer'
+        assert token.access_token is not None
+
+    def test_admin_get_me(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_admin_login: UserCreate
+    ):
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={
+                'username': user_test_admin_login.email, 'password': user_test_admin_login.password
+            }
+        )
+        assert res.status_code == HTTP_201_CREATED
+        token = AccessToken(**res.json())
+
+        headers = {
+            'Authorization': f'{token.token_type} {token.access_token}'
+        }
+        res = client.get(
+            app.url_path_for("users:get-user-me"),
+            headers=headers
+        )
+        assert res.status_code == HTTP_200_OK
+
+        user_me = UserPublic(**res.json())
+        assert user_test_admin_login.username == user_me.username
+        assert user_test_admin_login.email == user_me.email
+
+    def test_admin_access_restricted_endpoints(
+        self,
+        app: FastAPI,
+        client: TestClient,
+        user_test_admin_login: UserCreate
+    ):
+        res = client.post(
+            app.url_path_for("tokens:post-token"),
+            data={
+                'username': user_test_admin_login.email, 'password': user_test_admin_login.password
+            }
+        )
+        assert res.status_code == HTTP_201_CREATED
+        token = AccessToken(**res.json())
+
+        headers = {
+            'Authorization': f'{token.token_type} {token.access_token}'
+        }
+        res = client.get(
+            app.url_path_for("users:get-users"),
+            headers=headers
+        )
+        assert res.status_code == HTTP_200_OK
